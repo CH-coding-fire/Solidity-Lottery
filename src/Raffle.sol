991a138d08c930e2606d127b2fc1fd969c02b62a
chrisDesktop <chrisDesktop@gmail.com>
2024-06-03 23:31:36 +0800

    first

---
+++ src/Raffle.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+
+
+/**
+ * @title A sample Raffle Contract
+ * @author Chris
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2
+ */
+contract Raffle {
+    error Raffle__NotEnoughtEthSent();
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    //Events
+    event EnterRaffle(address indexed player);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+      ){
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_lastTimeStamp = block.timestamp;
+        
+    }
+
+    function enterRaffle() external payable {
+        if(msg.value < i_entranceFee){
+            revert Raffle__NotEnoughtEthSent();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnterRaffle(msg.sender);
+    }
+
+
+    function pickWinner() public {
+        if(block.timestamp - s_lastTimeStamp < i_interval){
+            revert();
+        }
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, //gas lane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS, 
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function getEntranceFee() external view returns(uint256) {
+        return i_entranceFee;
+    }
+}
+
+
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
